dc2d116d48d7e7a251231ee75eebbdce7f0c09bf
nbhattacharya08 <nilanjanbhattacharya22@gmail.com>
2023-11-23 00:19:56 +0530

    meow

--- src/components/Dmoney.jsx
+++ src/components/Dmoney.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
-
+import {useState , React} from 'react'
+import Web3 from 'web3';
+import axios from 'axios';
 function Dmoney() {
-  return (
-    <div>
-      Donate money
-    </div>
-  )
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [amount, setAmount] = useState(0)
+  const [charity, setCharity] = useState('')
+  const submit = async (e) => {
+    e.preventDefault()
+    const data = await axios.get(`http://localhost:4000/get-charity/${charity}`)
+    console.log(data.data.wallet)
+
+
+    if (!walletAddress) {
+      console.error('Wallet not connected');
+      return;
+    }
+
+    if (!amount || !charity) {
+      console.error('Please enter the amount and charity');
+      return;
+    }
+
+    try {
+      const web3 = new Web3(window.ethereum);
+
+      // Create a transaction object
+      const transactionObject = {
+        from: walletAddress,
+        to: data.data.wallet, // Replace with the receiver's wallet address
+        value: web3.utils.toWei(amount.toString(), 'ether'),
+        gas: 21000,
+      };
+
+      // Send the transaction using MetaMask popup
+      await web3.eth.sendTransaction(transactionObject);
+
+      alert('Transaction sent successfully');
+    } catch (error) {
+      alert('Failed to send transaction:', error);
+    }
+
+
+
+
+
+  }
+  const set = (e) => {
+    setAmount(e.target.value)
+    console.log(amount)
+  }
+  const setcharity = (e) => {
+    setCharity(e.target.value)
+    console.log(charity)
+  }
+  const handleConnectWallet = async (e) => {
+    e.preventDefault();
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        const address = accounts[0];
+        setWalletAddress(address);
+        console.log('Wallet connected:', address);
+      } catch (error) {
+        console.error('Failed to connect wallet:', error);
+      }
+    } else {
+      console.error('No wallet provider found');
+    }
+  }
+  if(!walletAddress){
+    return (
+      <div>
+        <form>
+          <label>Enter amount in MATIC</label>
+          <input className="border-2 border-blue-800" type="number" value={amount} onChange={set}/>
+          <label>Charity Name</label>
+          <input className="border-2 border-blue-800" type="text" value={charity} onChange={setcharity}/>
+          <button className="bg-blue-800 text-white px-4 py-2 rounded-md" onClick={submit} >Send</button>
+        </form>
+        <button className="bg-blue-800 text-white px-4 py-2 rounded-md" onClick={handleConnectWallet} >Connect Wallet</button>
+      </div>
+    )
+  }else{
+    return (
+      <div>
+        <form>
+          <label>Enter amount in MATIC</label>
+          <input className="border-2 border-blue-800" type="number" value={amount} onChange={set}/>
+          <label>Charity Name</label>
+          <input className="border-2 border-blue-800" type="text" value={charity} onChange={setcharity}/>
+          <button className="bg-blue-800 text-white px-4 py-2 rounded-md" onClick={submit} >Send</button>
+        </form>
+        <h1>Wallet Adress: {walletAddress}</h1>
+      </div>
+    )
+  }
 }
 
 export default Dmoney
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,113 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState, React } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Web3 from 'web3';
+
 function Home() {
-  return (
-    <div className='flex flex-row gap-6'>
-   <Link to="/donate-money">   <button className='border-2 border-blue-900 p-3 text-3xl'>Donate Money</button></Link>
-   <Link to="/donate-items">   <button className='border-2 border-blue-900 p-3 text-3xl'>Donate Items</button></Link>
-    </div>
-  )
+  const [name, setName] = useState('');
+  const [donor, setDonor] = useState('');
+  const [charity, setCharity] = useState('');
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [reg, setReg] = useState(false);
+
+  const handlename = (e) => {
+    setName(e.target.value);
+  };
+
+  const handledonor = (e) => {
+    setDonor(e.target.value);
+  };
+
+  const handlecharity = (e) => {
+    setCharity(e.target.value);
+  };
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    console.log('meow');
+    const data = await axios.post('http://localhost:4000/add', {
+      name: name,
+      donor: donor,
+      charity: charity,
+      wallet: walletAddress,
+    });
+    setReg(true)
+    console.log(data);
+  };
+
+  const handleConnectWallet = async (e) => {
+    e.preventDefault();
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        const address = accounts[0];
+        setWalletAddress(address);
+        console.log('Wallet connected:', address);
+      } catch (error) {
+        console.error('Failed to connect wallet:', error);
+      }
+    } else {
+      console.error('No wallet provider found');
+    }
+  };
+
+  if (reg) {
+    return (
+      <div className='flex flex-row gap-6'>
+      <Link to='/donate-money'>
+        <button className='border-2 border-blue-900 p-3 text-3xl'>Donate Money</button>
+      </Link>
+      <Link to='/donate-items'>
+        <button className='border-2 border-blue-900 p-3 text-3xl'>Donate Items</button>
+      </Link>
+      </div>
+    );
+  }else{
+    if(walletAddress){
+      return (
+        <div className='flex flex-row gap-6'>
+        
+        <div className='border-2 border-blue-900 p-3 text-3xl'>
+          <h1>Register</h1>
+          <form>
+            <input
+              type='text'
+              placeholder='Name'
+              className='border-2 border-blue-900 p-3 text-3xl'
+              onChange={handlename}
+            />
+            <label>
+              Donor
+              <input type='radio' name='gender' value='donor' onChange={handledonor} />
+            </label>
+            <label>
+              Charity
+              <input type='radio' name='gender' value='charity' onChange={handlecharity} />
+            </label>
+            <button className='border-2 border-blue-900 p-3 text-3xl' onClick={handlesubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+        <h1 className='border-2 border-blue-900 p-3 text-3xl'>Wallet Address: {walletAddress}</h1>
+      </div>
+      
+    );
+    }else{
+      return (
+        <div className='flex flex-row gap-6'>
+        <button className='border-2 border-blue-900 p-3 text-3xl' onClick={handleConnectWallet}>Connect Wallet</button>
+      </div>
+      
+    );
+    }
+  }
 }
 
-export default Home
+export default Home;
+
+
+
+
